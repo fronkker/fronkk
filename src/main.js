f9ac7adcd50a5dd621b63f17da80a39c480fae20
ShinHyeJi <liexil97@gmail.com>
2023-10-26 11:17:25 +0900

    feat: input export

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import FInput from "./components/FInput.vue";
-import FSelect from "./components/FSelect.vue";
-import FSeperator from "./components/FSeparator.vue";
+// import FInput from "./components/FInput.vue";
+// import FSelect from "./components/FSelect.vue";
+// import FSeperator from "./components/FSeparator.vue";
+import FkInput from "./components/FkInput.vue";
 
-export {FInput, FSelect, FSeperator}
+export {FkInput}
 
 // TODO 여기부터 개발용
-
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-
-const app = createApp(App)
-
-app.mount('#app')
+//
+// import { createApp } from 'vue'
+// import './style.css'
+// import App from './App.vue'
+//
+// const app = createApp(App)
+//
+// app.mount('#app')
