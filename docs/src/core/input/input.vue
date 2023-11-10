c4e1c220a5da539bd542ae7773489ff3f4dc8b86
ttap0704 <ttap0704@naver.com>
2023-11-10 13:36:58 +0900

    fix: prettier config 통일 & vue/js 파일 formatting

--- docs/src/core/input/input.vue
+++ docs/src/core/input/input.vue
@@ -2,32 +2,34 @@
   <div class="fk-guide--label">API</div>
   <div class="fk-guide--content">
     <div class="fk-guide--tabs">
-      <div class="fk-guide--tab clickable"
-           v-for="type of types"
-           @click="selectedType = type.id"
-           :class="selectedType === type.id && 'selected'"
-           v-text="type.value"
+      <div
+        class="fk-guide--tab clickable"
+        v-for="type of types"
+        @click="selectedType = type.id"
+        :class="selectedType === type.id && 'selected'"
+        v-text="type.value"
       />
     </div>
 
     <div class="fk-guide--tab-section">
       <div class="fk-guide--tab-section--left clickable">
-        <div v-for="content of contents[selectedType]"
-             :class="selectedContent === content.id && 'selected'"
-             @click="onClickContent(content)"
+        <div
+          v-for="content of contents[selectedType]"
+          :class="selectedContent === content.id && 'selected'"
+          @click="onClickContent(content)"
         >
           {{ content.value }}
         </div>
       </div>
 
       <div class="fk-guide--tab-section--right">
-        {{description}}
+        {{ description }}
       </div>
     </div>
   </div>
 </template>
 <script setup>
-import {ref} from "vue";
+import {ref} from 'vue'
 
 const selectedType = ref(0)
 const selectedContent = ref(0)
@@ -44,41 +46,41 @@ const types = [
   },
   {
     value: 'events',
-    id: 2
-  }]
+    id: 2,
+  },
+]
 const contents = {
   0: [
     {
       value: 'label',
-      id: 0
+      id: 0,
     },
     {
       value: 'modelValue',
-      id: 1
+      id: 1,
     },
   ],
   1: [
     {
       value: 'ㅎㅎ',
-      id: 0
+      id: 0,
     },
   ],
   2: [
     {
       value: 'ㅋㅋ',
-      id: 0
+      id: 0,
     },
-  ]
+  ],
 }
 const descriptions = {
   0: {
-    0: '하하...'
-  }
+    0: '하하...',
+  },
 }
 
 const onClickContent = ({id}) => {
   selectedContent.value = id
   description.value = descriptions[selectedType.value][selectedContent.value]
 }
-
 </script>
--- docs/src/main.js
+++ docs/src/main.js
@@ -1,6 +1,6 @@
 import '@fronkker/fronkk/dist/style.css'
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
